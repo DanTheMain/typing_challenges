348ddba703c8b47ee6997474a6c17c406fd43fe5
DanTheMain <Daniil.Morozov80@yandex.ru>
2023-09-11 14:03:24 +0300

    level_1 typing exercises follow up based on PR review

--- level_1/4.py
+++ level_1/4.py
@@ -8,4 +8,4 @@ def calculate_age(date_of_birth: datetime.date) -> int:
 
 
 if __name__ == "__main__":
-    assert calculate_age(date_of_birth=datetime.date(1965, 6, 2)) == 57 # nope
+    assert calculate_age(date_of_birth=datetime.date(1965, 6, 2)) == 57 # unlikely to work passed current year
--- level_1/9.py
+++ level_1/9.py
@@ -1,10 +1,9 @@
 import uuid
-from typing import Optional
 
 from constants import ___
 
 
-def is_correct_int(raw_int: Optional[str]) -> bool:
+def is_correct_int(raw_int: str | None) -> bool:
     pass
 
 
